c8813668d822600c445610cd45ad0d5021b7d996
mjulstro <mjulstro@macalester.edu>
2018-02-18 19:56:53 -0600

    flags wrong type & number of constructor arguments, won't touch primitives
    and the thing with the wrong number of method arguments is once again
    fixed!

--- java-type-checker/java_type_checker/expressions.py
+++ java-type-checker/java_type_checker/expressions.py
@@ -92,31 +92,37 @@ class MethodCall(Expression):
         Raises an error if the types don't check out. Doesn't return anything.
         """
 
-        expected_types = self.receiver.static_type().method_named(self.method_name).argument_types
-        call_name = self.receiver.declared_type.name + "." + self.method_name + names(expected_types)
-        actual_types = []
-        for item in self.args:
-            actual_types.append(item.static_type())
-
-        if len(actual_types) != len(expected_types):
+        if self.receiver.static_type() in [Type.void, Type.boolean, Type.int, Type.double, Type.null]:
             raise JavaTypeError(
-                "Wrong number of arguments for {0}: expected {1}, got {2}".format(
-                    call_name,
-                    len(expected_types),
-                    len(actual_types)))
+                "Type {0} does not have methods".format(self.receiver.static_type().name)
+            )
 
-        if expected_types != actual_types:
+        else:
+            expected_types = self.receiver.static_type().method_named(self.method_name).argument_types
+            call_name = self.receiver.declared_type.name + "." + self.method_name + "()"
+            actual_types = []
+            for item in self.args:
+                actual_types.append(item.static_type())
 
-            for i in range(0, len(expected_types)):
-                if expected_types[i] in actual_types[i].direct_supertypes:
-                    actual_types[i] = expected_types[i]
+            if len(actual_types) != len(expected_types):
+                raise JavaTypeError(
+                    "Wrong number of arguments for {0}: expected {1}, got {2}".format(
+                        call_name,
+                        len(expected_types),
+                        len(actual_types)))
 
             if expected_types != actual_types:
-                raise JavaTypeError(
-                    "{0} expects arguments of type {1}, but got {2}".format(
-                        call_name,
-                        names(expected_types),
-                        names(actual_types)))
+
+                for i in range(0, len(expected_types)):
+                    if expected_types[i] in actual_types[i].direct_supertypes:
+                        actual_types[i] = expected_types[i]
+
+                if expected_types != actual_types:
+                    raise JavaTypeError(
+                        "{0} expects arguments of type {1}, but got {2}".format(
+                            call_name,
+                            names(expected_types),
+                            names(actual_types)))
 
 class ConstructorCall(Expression):
     """
@@ -132,6 +138,35 @@ class ConstructorCall(Expression):
         all the possible values it could take on at runtime. Subclasses must implement this method.
         """
         return self.instantiated_type
+
+    def check_types(self):
+
+        if self.instantiated_type in [Type.void, Type.boolean, Type.int, Type.double, Type.null]:
+            raise JavaTypeError(
+                "Type {0} is not instantiable".format(self.instantiated_type.name)
+            )
+
+        else:
+            actual_types = []
+            for item in self.args:
+                actual_types.append(item.static_type())
+            expected_types = self.instantiated_type.constructor.argument_types
+
+            if len(actual_types) != len(expected_types):
+                raise JavaTypeError(
+                    "Wrong number of arguments for {0} constructor: expected {1}, got {2}".format(
+                        self.instantiated_type.name,
+                        len(expected_types),
+                        len(actual_types)
+                    )
+                )
+
+            if actual_types != expected_types:
+                raise JavaTypeError(
+                    "{0} constructor expects arguments of type {1}, but got {2}".format(
+                        self.instantiated_type.name,
+                        names(expected_types),
+                        names(actual_types) ))
 
 
 class JavaTypeError(Exception):
